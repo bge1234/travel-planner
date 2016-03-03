197d654d047cba0186f5ececaf25c8542e575f70
Ben Ebel <bge1234@gmail.com>
2016-03-03 14:57:41 -0700

    updated start path for node

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
 
-router.get('/api', function(req, res, next) {
+router.get('/', function(req, res, next) {
   knex('destinations').select().orderBy('id').then(function(results) {
     res.json(results);
   });
